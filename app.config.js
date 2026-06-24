0ff81c6eb340fe525baad1b2d38a61b51aed5882
Ojas Elawadhi <ojaselawadhi1@gmail.com>
2026-06-25 03:03:30 +0530

    version update

--- app.config.js
+++ app.config.js
@@ -29,7 +29,7 @@ module.exports = {
     },
     android: {
       package: "com.zenostudios.codewars",
-      versionCode: 26,
+      versionCode: 27,
       edgeToEdgeEnabled: true,
       predictiveBackGestureEnabled: false
     },
